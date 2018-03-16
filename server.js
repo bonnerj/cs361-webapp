c7d384e8f3c96ada1ed4291757eccb242220df49
bonnerj <32306277+bonnerj@users.noreply.github.com>
2018-03-16 16:11:31 -0400

    organized server file

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+
+/***************************************************************
+                      NODE MODULES
+****************************************************************/
 var express = require('express');
 var app = express();
 var nodemailer = require('nodemailer');
@@ -6,6 +10,10 @@ var handlebars = require('express-handlebars');
 var bodyParser = require("body-parser");
 var moment = require('moment');
 
+
+/*******************************************************
+ NODEMAILER - for verification email on account creation
+******************************************************/
 let transporter = nodemailer.createTransport({
   service: 'gmail',
   secure: false,
@@ -19,6 +27,10 @@ let transporter = nodemailer.createTransport({
   }
 });
 
+/***************************************************************
+                      Set up module usage
+****************************************************************/
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.set('port', 50113);
@@ -28,76 +40,114 @@ app.set("view engine", "handlebars");
 app.set('mysql', mysql);
 app.engine('handlebars', handlebars({
     defaultLayout: 'main',
-  //helpers: require(__dirname + "/public/js/helpers.js").helpers,        // Use helper functions if needed
+// Use helper functions if needed
+//helpers: require(__dirname + "/public/js/helpers.js").helpers,      
     partialsDir: __dirname + '/views/partials'
 }));
 
+/***************************************************************
+                      ROUTES
+****************************************************************/
+
+
+/*************************
+        STATIC ROUTES
+**************************/
+
+//default landing page
 app.get('/',function(req,res){
   res.render("homepage");
 });
 
+//resource links
 app.get('/resources',function(req,res){
   res.render("resources");
 });
 
+//registration page
 app.get('/register',function(req,res){
   res.render("registerForm");
 });
 
+//login page
 app.get('/userLogin', function(req,res){
   res.render("login")
 });
 
+//password reset
 app.get('/forgotPassword', function(req,res){
   res.render("forgotPass")
 })
 
+//homepage for employees
 app.get('/employee-home', function(req, res){
   res.render("employeePage");
 })
 
+//homepage for HR
 app.get('/hr-home', function(req, res){
   res.render("hrPage");
 })
 
+//file an incident report for employee
 app.get('/report', function(req, res){
   res.render("report");
 })
 
+//FAQs page
 app.get('/faqs', function(req, res){
   res.render("faqs");
 })
 
-app.post('/submitReport', function(req, res){
-  context = req.body;
-  context.reportDate = moment().format("YYYY-MM-DD");
-  console.log(context);
+/*****************************************
+              DYNAMIC ROUTES
+*****************************************/
 
-  res.render('reviewForm', context);
-})
 
-app.post('/submitted', function(req, res){
-  context = req.body;
-  console.log(context);
-  mysql.pool.query("INSERT INTO report (`reportDate`, `incidentDate`, `anonymous`, `accused`, `description`) VALUES (?,?,?,?,?)", 
-    [moment().format('YYYY-MM-DD'), context.incidentDate, context.anonymous, context.accused, context.description], function(err, result){
+/**********************
+    Account Creation  
+************************/
+
+app.get('/userregister',function(req,res){
+  console.log("got param:" + req.query.userName);
+  res.redirect("/");
+});
+
+//validates user upon registration
+app.post('/register/validate',function(req,res){
+  //console.log(req.body);
+  
+  //insert form values into table
+  mysql.pool.query("INSERT INTO employee (`username`, `email`, `pword`, " +
+    "`isValidated`, `failedAttempts`) VALUES (?,?,?,?,?)", 
+    [req.body.userName, req.body.email, req.body.password, 0, 0], 
+    function(err, result){
     if(err){
       next(err);
       return;
     }
-    console.log(result.insertId);
-    mysql.pool.query('SELECT * FROM report WHERE id=?', result.insertId, function(err, rows, fields){
-      if(err){
-      next(err);
-      return;
-      };
-      console.log(rows[0]);
-      res.render('formSent');
-    })
-  })
-})
+  });
+    
+  let HelperOptions = {
+    from: '"Group9" <noreply.group9@gmail.com',
+    to: req.body.email,
+    subject: 'Verfication Email',                                                                                         //identifier
+    text: 'Hello,\n\n' + 'Please verify your account by ' +
+          'clicking the link: \nhttp:\/\/' + req.headers.host + '\/userregister' 
+          + '?userName=' + req.body.userName + '.\n'
+  };
 
+  transporter.sendMail(HelperOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+  });
+  res.render("validate");
+});
 
+/**********************
+      Login Route 
+************************/
 app.post('/login', function(req,res){
   var context = {}; 
   var msg = {};
@@ -106,7 +156,8 @@ app.post('/login', function(req,res){
   var password = req.body.password;
   
   // query database for username
-  mysql.pool.query('SELECT * FROM employee WHERE username=?', [req.body.userName], function(err, rows, fields){
+  mysql.pool.query('SELECT * FROM employee WHERE username=?', 
+    [req.body.userName], function(err, rows, fields){
     if(err){
       next(err);
       return;
@@ -125,7 +176,8 @@ app.post('/login', function(req,res){
     else {
       // Check if user failed to login 5 times already
       if (rows[0].failedAttempts >= 5) {
-        msg.status = "The referenced account is now locked. Please contact your administrator.";
+        msg.status = "The referenced account is now locked. " +
+                      "Please contact your administrator.";
         res.render('login', msg);
       }
       // if password matches, print success
@@ -133,7 +185,8 @@ app.post('/login', function(req,res){
       {
         // print success - reset failedAttempts
         console.log("login success!");
-        mysql.pool.query('UPDATE employee SET failedAttempts=? WHERE username=?', 
+        mysql.pool.query('UPDATE employee SET failedAttempts=? ' + 
+                         'WHERE username=?', 
           [0, rows[0].username], function(err, rows, fields){
           if(err){
             next(err);
@@ -148,7 +201,8 @@ app.post('/login', function(req,res){
       else {
         // login failed - increment failedAttempts
         console.log("login failed");
-        mysql.pool.query('UPDATE employee SET failedAttempts=? WHERE username=?', 
+        mysql.pool.query('UPDATE employee SET failedAttempts=?' + 
+                         'WHERE username=?', 
           [rows[0].failedAttempts + 1, rows[0].username], function(err){
           if(err){
             next(err);
@@ -156,7 +210,8 @@ app.post('/login', function(req,res){
           }
           // If too many failed attempts, send message that account is now locked
           if (rows[0].failedAttempts >= 4) {
-            msg.status = "The referenced account is now locked. Please contact your administrator.";
+            msg.status = "The referenced account is now locked. " +
+                         "Please contact your administrator.";
             res.render('login', msg);
           }
           else  {
@@ -169,56 +224,23 @@ app.post('/login', function(req,res){
   });
 });
 
-app.post('/profileEdit',function(req,res){
-   var context = {};
-	
-   mysql.pool.query("SELECT * FROM employee WHERE id=?", [req.body.id], function(err, result){
-    if(err){
-      next(err);
-      return;
-    }
-    if(result.length == 1){
-      var curVals = result[0];
-      console.log(curVals);
-      mysql.pool.query("UPDATE employee SET username=?, email=?, pword=? WHERE id=? ",
-        [req.body.userName || curVals.username, req.body.email || curVals.email, req.body.password || curVals.pword, curVals.id],
-        function(err, result){
-        if(err){
-          next(err);
-          return;
-        }
-        mysql.pool.query("SELECT * FROM employee WHERE id=?", [req.body.id], function(err, rows, fields){
-          if(err){
-          next(err);
-          return;
-        }
-        context=rows[0]
-        console.log(context);
-
-        //context.results = "Updated " + result.changedRows + " rows.";
-        res.render('profile', context);
-        });
-      });
-    }
-  });
-});
-
-
 app.post('/forgotPass/reset',function(req,res){
   //console.log(req.body);
    var newPassword = req.body.password;
    
   // query database for username
-  mysql.pool.query('SELECT * FROM employee WHERE username=? AND email=?', [req.body.userName, req.body.email], function(err, rows, fields){
-    if(err){
-      next(err);
-      return;
-    }
+  mysql.pool.query('SELECT * FROM employee WHERE username=? AND email=?', 
+    [req.body.userName, req.body.email], 
+    function(err, rows, fields){
+      if(err){
+        next(err);
+        return;
+      }
 
     context = rows[0];
     console.log(rows[0]);
 
-	// if username DNE, error
+  // if username DNE, error
     if (!rows) {
        // error - username/email combo does not exist
        console.log("error: username/email combination not found");
@@ -226,51 +248,104 @@ app.post('/forgotPass/reset',function(req,res){
 
     // if username/email is found 
     else {
-      mysql.pool.query("UPDATE employee SET pword=? WHERE username=? AND email=?", [newPassword, rows[0].username, rows[0].email], function(err, rows, fields){
-        if(err){
-          next(err);
-          return;
-        }
+      mysql.pool.query("UPDATE employee SET pword=? WHERE username=? AND email=?", 
+        [newPassword, rows[0].username, rows[0].email], 
+        function(err, rows, fields){
+          if(err){
+            next(err);
+            return;
+          }
       })
     }
 
     res.redirect("/userLogin");
 
-	  });
+    });
 });
 
+/************************
+    Incident Report  
+************************/
+//generates preview of incident report
+app.post('/submitReport', function(req, res){
+  context = req.body;
+  context.reportDate = moment().format("YYYY-MM-DD");
+  console.log(context);
 
-app.post('/register/validate',function(req,res){
-  //console.log(req.body);
-  
-  //insert form values into table
-  mysql.pool.query("INSERT INTO employee (`username`, `email`, `pword`, `isValidated`, `failedAttempts`) VALUES (?,?,?,?,?)", [req.body.userName, req.body.email, req.body.password, 0, 0], function(err, result){
-    if(err){
-      next(err);
-      return;
-    }
-  });
-    
+  res.render('reviewForm', context);
+})
 
-  let HelperOptions = {
-    from: '"Group9" <noreply.group9@gmail.com',
-    to: req.body.email,
-    subject: 'Verfication Email',                                                                                         //identifier
-    text: 'Hello,\n\n' + 'Please verify your account by clicking the link: \nhttp:\/\/' + req.headers.host + '\/userregister' + '?userName=' + req.body.userName + '.\n'
-  };
+//submits incident report
+app.post('/submitted', function(req, res){
+  context = req.body;
+  console.log(context);
+  mysql.pool.query("INSERT INTO report " +
+                  "(`reportDate`, `incidentDate`, `anonymous`, `accused`, `description`) "+
+                  "VALUES (?,?,?,?,?)", 
+    [moment().format('YYYY-MM-DD'), 
+    context.incidentDate, context.anonymous, context.accused, context.description], 
+    function(err, result){
+      if(err){
+        next(err);
+        return;
+      }
+    console.log(result.insertId);
+    mysql.pool.query('SELECT * FROM report WHERE id=?', result.insertId, 
+      function(err, rows, fields){
+        if(err){
+        next(err);
+        return;
+        };
+      console.log(rows[0]);
+      res.render('formSent');
+    })
+  })
+})
 
-  transporter.sendMail(HelperOptions, (error, info) => {
-    if (error) {
-      return console.log(error);
-    }
-  });
-  res.render("validate");
+
+/************************
+    Employee Pages  
+************************/
+
+app.post('/profileEdit',function(req,res){
+   var context = {};
+	
+   mysql.pool.query("SELECT * FROM employee WHERE id=?", [req.body.id], 
+    function(err, result){
+      if(err){
+        next(err);
+        return;
+      }
+      if(result.length == 1){
+        var curVals = result[0];
+        console.log(curVals);
+        mysql.pool.query("UPDATE employee SET username=?, email=?, pword=? WHERE id=? ",
+          [req.body.userName || curVals.username, req.body.email || curVals.email, req.body.password || curVals.pword, curVals.id],
+          function(err, result){
+          if(err){
+            next(err);
+            return;
+          }
+          mysql.pool.query("SELECT * FROM employee WHERE id=?", [req.body.id], 
+            function(err, rows, fields){
+              if(err){
+              next(err);
+              return;
+              }
+          context=rows[0]
+          console.log(context);
+
+          //context.results = "Updated " + result.changedRows + " rows.";
+          res.render('profile', context);
+          });
+        });
+      }
+    });
 });
 
-app.get('/userregister',function(req,res){
-  console.log("got param:" + req.query.userName);
-  res.redirect("/");
-});
+/************************
+      Error Handling 
+************************/
 
 app.use(function(req,res){
   res.type('text/plain');
@@ -286,5 +361,6 @@ app.use(function(err, req, res, next){
 });
 
 app.listen(app.get('port'), function(){
-  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+  console.log('Express started on http://localhost:' + app.get('port') + 
+    '; press Ctrl-C to terminate.');
 });
